02a752ed8c7db0f50258c04faafc0478d6f01695
randika0514 <randikadimantha@gmail.com>
2022-07-12 12:23:47 +0530

    Updated btnSaveOnAction method

--- src/controller/StudentManageFormController.java
+++ src/controller/StudentManageFormController.java
@@ -1,10 +1,18 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import model.Student;
+import util.CrudUtil;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class StudentManageFormController {
 
@@ -30,7 +38,25 @@ public class StudentManageFormController {
     public void btnDeleteOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnSaveOnAction(ActionEvent actionEvent) {
+    public void btnSaveOnAction(ActionEvent actionEvent) throws IOException {
+        Student s = new Student(
+                txtStuId.getText(),txtStuName.getText(),txtStuEmail.getText(),txtStuContact.getText(),txtStuAddress.getText(),txtStuNic.getText()
+        );
+
+        try {
+            if (CrudUtil.execute("INSERT INTO ijse.student VALUES (?,?,?,?,?,?)",s.getStuId(),s.getStuName(),s.getStuEmail(),s.getContact(),s.getAddress(),s.getNic())){
+                new Alert(Alert.AlertType.CONFIRMATION,"Saved").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        studentContext.getChildren().clear();
+        Parent parent = FXMLLoader.load(getClass().getResource("../view/StudentManageForm.fxml"));
+        studentContext.getChildren().add(parent);
+
     }
 
     public void btnSearch(ActionEvent actionEvent) {
