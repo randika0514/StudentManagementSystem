b1a15af170b2ed82882f5c78c95c6d7b4c9eb726
randika0514 <randikadimantha@gmail.com>
2022-07-12 11:00:48 +0530

    Updated Student model

--- src/controller/StudentManageFormController.java
+++ src/controller/StudentManageFormController.java
@@ -1,5 +1,38 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentManageFormController {
 
+    public AnchorPane studentContext;
+    public TextField txtStuId;
+    public TextField txtStuName;
+    public TextField txtStuNic;
+    public TextField txtStuAddress;
+    public TextField txtStuContact;
+    public TextField txtStuEmail;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+    public TextField txtSearch;
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearch(ActionEvent actionEvent) {
+    }
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,4 +1,82 @@
 package model;
 
 public class Student {
+    private String stuId;
+    private String stuName;
+    private String stuEmail;
+    private String contact;
+    private String address;
+    private String nic;
+
+    public Student() {
+    }
+
+    public Student(String stuId, String stuName, String stuEmail, String contact, String address, String nic) {
+        this.stuId = stuId;
+        this.stuName = stuName;
+        this.stuEmail = stuEmail;
+        this.contact = contact;
+        this.address = address;
+        this.nic = nic;
+    }
+
+    public String getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(String stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public String getStuEmail() {
+        return stuEmail;
+    }
+
+    public void setStuEmail(String stuEmail) {
+        this.stuEmail = stuEmail;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "stuId='" + stuId + '\'' +
+                ", stuName='" + stuName + '\'' +
+                ", stuEmail='" + stuEmail + '\'' +
+                ", contact='" + contact + '\'' +
+                ", address='" + address + '\'' +
+                ", nic='" + nic + '\'' +
+                '}';
+    }
 }
